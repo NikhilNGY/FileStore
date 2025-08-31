eef0b6467b2fd9deb071d93644e4dd10510d3005
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-08-31 17:48:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ BAN_SUPPORT = os.environ.get("BAN_SUPPORT", "https://t.me/Nikhil5757h")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "200"))
 #--------------------------------------------
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/4466d37d43f5703516f74.jpg")
-FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://envs.sh/etM.jpg")
 #--------------------------------------------
 
 #--------------------------------------------
