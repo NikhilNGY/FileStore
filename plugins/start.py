e893cba2779b1ec4f20b4d85f321f837586de1d6
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-08-31 15:49:10 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -138,17 +138,26 @@ async def start_command(client: Client, message: Message):
     ]
             ]
         )
-        await message.reply_photo(
-            photo=START_PIC,
-            caption=START_MSG.format(
-                first=message.from_user.first_name,
-                last=message.from_user.last_name,
-                username=None if not message.from_user.username else '@' + message.from_user.username,
-                mention=message.from_user.mention,
-                id=message.from_user.id
-            ),
-            reply_markup=reply_markup,
-            message_effect_id=5104841245755180586)  # 🔥
+        caption = START_MSG.format(
+    first=message.from_user.first_name,
+    last=message.from_user.last_name,
+    username=None if not message.from_user.username else '@' + message.from_user.username,
+    mention=message.from_user.mention,
+    id=message.from_user.id
+)
+
+if START_PIC:  # If a photo is set
+    await message.reply_photo(
+        photo=START_PIC,
+        caption=caption,
+        reply_markup=reply_markup,
+        message_effect_id=5104841245755180586
+    )
+else:  # Fallback to text only
+    await message.reply_text(
+        text=caption,
+        reply_markup=reply_markup
+    )  # 🔥
         
         return
 
